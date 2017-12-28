bb030ae9c6272eaea42a442bd511febcf88f0d8e
Kevin <cho.kevin@hotmail.com>
2017-12-28 00:20:56 +0000

    contact page w/ maps complete

---
+++ src/components/contact-maps.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+
+const MapWithAMarker = withGoogleMap(props => (
+  <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+    <Marker position={{ lat: -34.397, lng: 150.644 }} />
+  </GoogleMap>
+));
+
+export default MapWithAMarker;
