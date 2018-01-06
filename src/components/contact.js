0221144d27dd2193943d6ca7efebf43c373dd691
Kevin <cho.kevin@hotmail.com>
2018-01-06 15:25:37 +0800

    contact page bulma styling complete

--- src/components/contact.js
+++ src/components/contact.js
@@ -9,33 +9,87 @@ class Contact extends Component {
   }
 
   render() {
-    const landingBackground = {
-      width: "100vw",
-      height: "100vh"
-    };
-
     return (
-      <div className="container-fluid" style={landingBackground}>
+      <div className="container-fluid">
         <div className="container">
           <br />
-          <h1>Contact Us</h1>
-          <br />
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-          </p>
-          <br />
-          <p>Address: xxx, xxx, xxx, hong kong</p>
-          <p>Email: xxx@gmail.com</p>
-          <br />
-          <MapWithAMarker
-            containerElement={<div style={{ height: `400px` }} />}
-            mapElement={<div style={{ height: `80%` }} />}
-          />
+
+          <section class="articles">
+            <div class="column is-8 is-offset-2" style={{ padding: "0px" }}>
+              <section class="hero is-danger is-bold is-small promo-block">
+                <div class="hero-body">
+                  <div class="container">
+                    <h1 class="title">
+                      <i class="fa fa-bell-o" />
+                      Contact Us
+                    </h1>
+                  </div>
+                </div>
+              </section>
+              <br />
+              <div class="card article">
+                <div class="card-content">
+                  <div class="content article-body">
+                    <p style={{ fontSize: "15px", textAlign: "justify" }}>
+                      Molestie ac feugiat sed lectus vestibulum. Feugiat sed
+                      lectus vestibulum mattis. Volutpat diam ut venenatis
+                      tellus in metus vulputate. Feugiat in fermentum posuere
+                      urna nec. Pharetra convallis posuere morbi leo urna
+                      molestie at. Accumsan lacus vel facilisis volutpat est
+                      velit egestas. Fermentum leo vel orci porta. Faucibus
+                      interdum posuere lorem ipsum.
+                    </p>
+                    <table
+                      class="table"
+                      style={{
+                        borderSpacing: "0px 10px",
+                        borderCollapse: "separate"
+                      }}
+                    >
+                      <tbody>
+                        <tr
+                          style={{
+                            backgroundColor: "#00d1b2"
+                          }}
+                        >
+                          <td>
+                            <i class="fa fa-envelope" aria-hidden="true" />
+                          </td>
+                          <td>
+                            <p
+                              style={{ fontSize: "15px", textAlign: "justify" }}
+                            >
+                              kenChicken@gmail.com
+                            </p>
+                          </td>
+                        </tr>
+                        <tr
+                          style={{
+                            backgroundColor: "#00d1b2"
+                          }}
+                        >
+                          <td>
+                            <i class="fa fa-map-marker" aria-hidden="true" />
+                          </td>
+                          <td>
+                            <p
+                              style={{ fontSize: "15px", textAlign: "justify" }}
+                            >
+                              4/F, Old Bank of China Building, Bank Street
+                            </p>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <MapWithAMarker
+                      containerElement={<div style={{ height: `300px` }} />}
+                      mapElement={<div style={{ height: `100%` }} />}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
       </div>
     );
